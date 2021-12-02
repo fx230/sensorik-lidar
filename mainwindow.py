750d998b2c7e2330326876b3b96253b18558900c
Flixa <flixa.fr@googlemail.com>
2021-12-02 15:17:45 +0100

    second draft of gui with slider

--- mainwindow.py
+++ mainwindow.py
@@ -13,31 +13,48 @@ from tkinter import ttk
 root = Tk()
 
 content = ttk.Frame(root)
-frame = ttk.Frame(content, borderwidth=5, relief="ridge", width=200, height=100)
-namelbl = ttk.Label(content, text="Name")
+frame = ttk.Frame(content, borderwidth=6, relief="ridge", width=400, height=200)
+namelbl = ttk.Label(content, text="LiDar Signalgenerator")
+namelbl_sensorrange = ttk.Label(content, text="Sensorrange")
 name = ttk.Entry(content)
 
-i_number_of_layers = 1
-b_reflexion = BooleanVar(value=False)
-b_cushioning = BooleanVar(value=True)
-b_doppler = BooleanVar(value=True)
 
-one = ttk.Checkbutton(content, text="Number of Layers", variable=i_number_of_layers, onvalue=True)
+##########Declarations########################
+i_number_of_layers = BooleanVar(value=False)
+b_reflexion = BooleanVar(value=False)
+b_cushioning = BooleanVar(value=False)
+b_doppler = BooleanVar(value=False)
+f_sensorrange = 70.00
+f_field_of_view = 1.00
+
+########### Slider ###########################
+sensorrange = Scale(content, from_=20.00, to=70.00, orient=HORIZONTAL)
+#filedofview = Scale(content, from_=0.00, to=10.00, variable=f_field_of_view)
+
+###########Radiobuttons#####################
+one = ttk.Checkbutton(content, text="Two layer", variable=i_number_of_layers, onvalue=True)
 two = ttk.Checkbutton(content, text="Reflexion", variable=b_reflexion, onvalue=True)
 three = ttk.Checkbutton(content, text="Cushioning", variable=b_cushioning, onvalue=True)
 four = ttk.Checkbutton(content, text="Doppler", variable=b_doppler, onvalue=True)
+
+############Buttons###################
 ok = ttk.Button(content, text="Generate")
 cancel = ttk.Button(content, text="Exit", command=sys.exit)
 
+
+
+###############position#################
 content.grid(column=0, row=0)
-frame.grid(column=0, row=0, columnspan=3, rowspan=2)
-namelbl.grid(column=3, row=0, columnspan=2)
-name.grid(column=3, row=1, columnspan=2)
-one.grid(column=0, row=3)
-two.grid(column=1, row=3)
-three.grid(column=2, row=3)
-ok.grid(column=3, row=3)
-cancel.grid(column=4, row=3)
+namelbl.grid(column=0, row=0, columnspan=2)
+#name.grid(column=0, row=1, columnspan=2)
+
+sensorrange.grid(column=1, row=2)
+one.grid(column=1, row=3)
+two.grid(column=1, row=4)
+three.grid(column=1, row=5)
+four.grid(column=1, row=6)
+ok.grid(column=2, row=7)
+cancel.grid(column=3, row=7)
 
 root.mainloop()
 
